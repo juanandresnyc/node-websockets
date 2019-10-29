f6dfe18981f13a7e517677003a75909024766582
juan andres andrango <andrango.juanandres@gmail.com>
2019-10-28 22:25:40 -0400

    fix server. show players

--- server.js
+++ server.js
@@ -7,18 +7,31 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const INDEX = path.join(__dirname, 'index.html');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+let server = express()
+  .use((req, res) => res.sendFile(INDEX) );
+
+// for dev only
+if (process.env.LOCAL_DEV_SSL) {
+  const https = require('https');
+  const fs = require('fs');
+  server = https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+  }, server)
+}
+
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+// TODO Support accounts!
+let ids = 11;
 
 const wss = new SocketServer({ server });
-let ids = 11;
 wss.on('connection', (ws) => {
   ws._ID = ids;
   ids += 1;
+  console.log('new client connected', ws._ID);
   ws.on('message', function incoming(message) {
     wss.clients.forEach((client) => {
-      console.log('each client', client._ID);
       if (ws._ID !== client._ID) {
         client.send(message);
       }
